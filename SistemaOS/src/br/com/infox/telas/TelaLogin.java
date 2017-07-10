cfd1fa0ca69937c5f8d5f02ad46b4bf548e2cbef
DJAIR VIEIRA <djairv53@gmail.com>
2017-07-09 23:40:48 -0300

    Construcao da tela de Login

--- SistemaOS/src/br/com/infox/telas/TelaLogin.java
+++ SistemaOS/src/br/com/infox/telas/TelaLogin.java
@@ -6,10 +6,21 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JTextArea;
+import java.awt.Button;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Color;
+import java.awt.Label;
+import java.awt.TextArea;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 
 public class TelaLogin extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
 
 	/**
 	 * Launch the application.
@@ -32,11 +43,38 @@ public class TelaLogin extends JFrame {
 	 */
 	public TelaLogin() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 494, 321);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		
+		Button button = new Button("Login");
+		button.setBackground(Color.GRAY);
+		button.setActionCommand("btnLogin");
+		button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		contentPane.add(button, BorderLayout.SOUTH);
+		
+		Label label = new Label("Usu\u00E1rio:");
+		label.setBackground(Color.GRAY);
+		contentPane.add(label, BorderLayout.WEST);
+		
+		Label label_1 = new Label("Senha:");
+		label_1.setBackground(Color.GRAY);
+		contentPane.add(label_1, BorderLayout.EAST);
+		
+		textField = new JTextField();
+		textField.setBackground(Color.ORANGE);
+		contentPane.add(textField, BorderLayout.NORTH);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setBackground(Color.GREEN);
+		contentPane.add(textField_1, BorderLayout.CENTER);
+		textField_1.setColumns(10);
 	}
 
 }
